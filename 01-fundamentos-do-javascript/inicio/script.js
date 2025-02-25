7de1189589247a25467e2b458256795f230d12d9
rafaelamachadocamara <rafaelamachado.camara@yahoo.com.br>
2025-02-25 20:00:34 -0300

    separando o js do html

---
+++ 01-fundamentos-do-javascript/inicio/script.js
@@ -0,0 +1,19 @@
+/*   Alerta de Olá Mundo
+         alert("Olá Mundo");
+ 
+         console.log("Olá Mundo");
+ 
+         let mensagem = "Olá Mundo";
+  
+         console.log(mensagem);
+  
+         let nome = "Rafaela";
+         console.log("Olá," + nome + "!"); */
+
+// Adição
+let soma = 5 + 3;
+console.log("A soma é ", soma); // 8
+
+// Subtração
+let subtracao = 10 - 4;
+console.log("A subtração é ", subtracao); // 6
